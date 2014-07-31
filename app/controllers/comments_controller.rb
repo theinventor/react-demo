83a0a40f0423fa543f3ff77d710c060f3d120203
Troy Anderson <the.inventor@gmail.com>
2014-07-31 08:08:04 -0700

    initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  
+  respond_to :json
+
+   def index
+     respond_with Comment.all
+   end
+
+   def create
+     sleep 3
+     respond_with Comment.create(comment_params)
+   end
+
+   private
+
+   def comment_params
+     params.require(:comment).permit(:author, :comment)
+   end
+   
+   
+end
